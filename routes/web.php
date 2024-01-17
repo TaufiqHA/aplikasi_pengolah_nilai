cf49b69b8fd5482a6250be8d3b313206b1dc08ec
Taufiq <htaufiq225@gmail.com>
2024-01-17 23:36:57 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// login
+Route::get('login', [LoginController::class, 'index'])->name('login.index');
+
+// register
+Route::get('register', [RegisterController::class, 'index'])->name('register.index');
